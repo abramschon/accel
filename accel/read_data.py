c894286971d20443634e92aebb0c7f50bc06abbb
Bram <abram.schonfeldt@kellogg.ox.ac.uk>
2021-12-09 20:10:10 +0000

    Removes old func and includes subsets

--- accel/read_data.py
+++ accel/read_data.py
@@ -1,30 +1,16 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-
-def load_xy(file_path : str, 
-            x_names : [str, ...],
-            y_name : str
-           ):
-    """ Loads data and selects provided feature and response names """
-    data = pd.read_csv(file_path)
-    X = data[x_names]
-    y = data[y_name]
-    return X, y
 
 def load_data(file_path : str):
     """ Utility function to load the data files and return pd dataframe """
     return pd.read_csv(file_path)
 
-def dataset_split(X : pd.DataFrame, 
-                  y : pd.Series, 
-                  perc_test : float):
-    """ 
-        Function to split the dataset into train and test 
-        where perc_test is how the percentage of the total data used as test data
-    """
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=perc_test)
-    return X_train, X_test, y_train, y_test
+def select_sets(df : pd.DataFrame, 
+                *sets : set, # as many as you want
+               ):
+    """ Selects the features in the union of the provided sets """
+    union = set.union(*sets)
+    return df[union]
 
 def to_numpy_cont(X : pd.DataFrame):
     """ 
